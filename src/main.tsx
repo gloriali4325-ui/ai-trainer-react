829949a1c904d36343633beef6948c855c71ca82
Li Gloria <242357943+gloriali4325-ui@users.noreply.github.com>
2026-02-09 15:43:09 +0800

    fix 404 error

--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,9 @@ import { AppProvider } from './state';
 import { App } from './App';
 import './styles/global.css';
 
-const basename = ((import.meta as unknown) as { env: { BASE_URL: string } }).env.BASE_URL || '/ai-trainer-react/';
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter basename={basename}>
+    <BrowserRouter>
       <AppProvider>
         <App />
       </AppProvider>
