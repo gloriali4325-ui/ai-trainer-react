abd2120363531b0b7a8bf61202b0775b446c0344
Li Gloria <242357943+gloriali4325-ui@users.noreply.github.com>
2026-02-09 15:53:58 +0800

    fix issue

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,36 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Navigate } from 'react-router-dom';
 import { AppProvider } from './state';
 import { App } from './App';
 import './styles/global.css';
 
+function Root() {
+  const [redirect, setRedirect] = React.useState<string | null>(null);
+
+  useEffect(() => {
+    const redirect = sessionStorage.redirect;
+    if (redirect) {
+      delete sessionStorage.redirect;
+      setRedirect(redirect);
+    }
+  }, []);
+
+  if (redirect) {
+    return <Navigate to={`/${redirect}`} replace />;
+  }
+
+  return (
+    <AppProvider>
+      <App />
+    </AppProvider>
+  );
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter basename="/ai-trainer-react/">
-      <AppProvider>
-        <App />
-      </AppProvider>
+      <Root />
     </BrowserRouter>
   </React.StrictMode>,
 );
