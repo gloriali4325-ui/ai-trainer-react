2fd7794d2000c6917ce41c1baf18c4c4c6e8b5d9
Li Gloria <242357943+gloriali4325-ui@users.noreply.github.com>
2026-02-09 15:51:57 +0800

    fix issue

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import './styles/global.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/ai-trainer-react/">
       <AppProvider>
         <App />
       </AppProvider>
